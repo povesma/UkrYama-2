e4e140d1681b8d5e404bc2f8112e7283a48f03a9
Dmitry Povesma <povesma@gmail.com>
2014-05-22 22:07:02 +1200

    Donate page update: PrivatBank card number

--- protected/views/site/pages/donate.php
+++ protected/views/site/pages/donate.php
@@ -8,31 +8,12 @@ $this->layout='//layouts/header_blank';
     <br />
   
     <br />
-   	
-    <h3>Банковский перевод</h3>
-  
-    <br />
-   	<strong>Получатель</strong>: АГОО «Новус»
-    <br />
-   	<strong>Банк получателя</strong>: Крымское РУ ПриватБанка г. Симферополь
-    <br />
-   	<strong>МФО</strong>: 384436
-    <br />
-   	<strong>ОКПО</strong>: 37252660
-    <br />
-   	<strong>Счет получателя в банке</strong>: 26001054911047
-    <br />
-   	<strong>Назначение платежа</strong>: Добровольное пожертвование на уставную деятельность (НДС не облагается)
-    <br />
+Сделайте перевод на карту ПриватБанка: 4149 4978 0285 8862 на імя Кравцов Григорий Юрьйевич (координатор проекта УкрЯма).
    	
     <br />
   
     <br />
-   	пользователи Webmoney могут перевести нам на счет любую сумму (WMU) через этот <a href="http://ukrbuy.com/payment/directlist/direct?id=97&amp;amount=" target="_blank" >сервис</a>. <img src="http://ukryama.info/uploads/images/00/00/02/2011/11/02/3a197b.png" align="left"  /> Перечисленные средства попадут прямо на наш банковский счет. Мы пока не решились завести Webmoney-кошелек, т. к. с точки зрения бухгалтерии могут быть нестыковки. <i>Комиссия этого сервиса составляет 1,5% (но не менее 3 грн).</i> 	
-    <br />
   
-    <br />
-   	<iframe height="133" src="https://money.yandex.ru/embed/donate.xml?uid=410011204487454&amp;default-sum=10&amp;targets=%d0%9d%d0%b0+%d1%80%d0%b0%d0%b7%d0%b2%d0%b8%d1%82%d0%b8%d0%b5+%d0%bf%d1%80%d0%be%d0%b5%d0%ba%d1%82%d0%b0+%c2%ab%d0%a3%d0%ba%d1%80%d0%af%d0%bc%d0%b0%c2%bb&amp;target-visibility=on&amp;project-name=%c2%ab%d0%a3%d0%ba%d1%80%d0%af%d0%bc%d0%b0%c2%bb&amp;project-site=http%3a%2f%2fukryama.com%2f&amp;button-text=01&amp;hint=&amp;fio=on" frameborder="0" width="450" scrolling="no"></iframe>
     <br />
   
     <br />
