da4ad2fe1313466ae7cdb89e7b9b60b02664ea26
unknown <povesma@gmail.com>
2017-04-12 01:29:31 +1200

    VK fix

--- protected/views/layouts/blank.php
+++ protected/views/layouts/blank.php
@@ -14,7 +14,7 @@
 <!--[if lte IE 7]><link rel="stylesheet" href="/css/ie.css" type="text/css" /><![endif]-->
 
 
-<script type="text/javascript" src="http://userapi.com/js/api/openapi.js?22"></script>
+<script type="text/javascript" src="//vk.com/js/api/openapi.js?22"></script>
 
 <script type="text/javascript">VK.init({apiId: 2232074, onlyWidgets: true});</script>
 
