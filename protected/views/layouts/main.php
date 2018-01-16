e8667161e4c576f057056dbe6c6bdb6ac1e27fbc
Evgeniy Poremchuk <poremchuk@users.noreply.github.com>
2018-01-16 20:47:35 +0200

    Update main.php
    Change footer

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -175,7 +175,7 @@ Yii::app()->clientScript->registerCssFile(Yii::app()->clientScript->getCoreScrip
 			<p class="rosyama"><noindex><a class="rs" target="_blank" href="http://rosyama.ru/" title="РосЯма">РосЯма</a></noindex><br/>Яму мне запили!<br/></p>
 			<p class="copy">Дороги — наша відповідальність. Контролюй це!<br />
    			Хостинг — «<noindex><a href="http://www.ukraine.com.ua/" target="_blank" rel="nofollow">Україна</a></noindex>»<br />
-   			<span class="studio-copyright">Дизайн — веб-студия <a href="http://stfalcon.com" target="_blank"><span class="icon"></span>stfalcon.com</a></span>
+   			<!--  <span class="studio-copyright">Дизайн — веб-студия <a href="http://stfalcon.com" target="_blank"><span class="icon"></span>stfalcon.com</a></span>-->  
 			Идея — <noindex><a href="http://navalny.ru/" rel="nofollow" target="_blank">Алексей Навальный</a></noindex>, 2011<br />
 <!--   			Разработано в <noindex><a href="http://pixelsmedia.ru" rel="nofollow" target="_blank">Pixelsmedia</a> </noindex>на Yii.<br/>
                            Поддержка сайта <noindex><a href="http://force-it.org" rel="nofollow" target="_blank">force-it.org</a></noindex>.<br/>
